1d350f88a533efec77f0cb9675f6dfbb10c4d2e3
shendel <im_shendel@inbox.ru>
2021-09-17 14:33:27 +0300

    createAddress example

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import CryptoInterface from './swap.io-keys/src/crypto/interface'
 
 const cInterface = new CryptoInterface()
+window.cInterface = cInterface
 const testMnemonic = "talk crisp crane dose network winner lumber harvest actual brand loud patch achieve army turtle warm section grab wish traffic anger steak rate square"
 cInterface
   .createProfileFromMnemonic(
@@ -15,6 +16,23 @@ cInterface
   )
   .then((testProfile) => {
     console.log('>>> testProfile', testProfile)
+    // get network adaptor
+    cInterface
+      .getNetworkAdaptor('bitcoin')
+      .then((bitcoinNetworkAdaptor) => {
+        // create address
+        const addressInfo = testProfile.createAddress(
+          bitcoinNetworkAdaptor,
+          0, // address index -0,1,2 etc
+          // params
+          {
+            template: 'default', // bip44, other - address type
+            templateSetting: {} // template settings
+          }
+        )
+        console.log('>>> addressInfo', addressInfo)
+
+      })
   })
 
 
